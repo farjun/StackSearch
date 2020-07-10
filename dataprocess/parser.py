47147ab958e9184f45ef5576cf759d7e7866cb5d
omer <omerbh1@gmail.com>
2020-07-10 11:57:18 +0300

    implemented Post iterator added preproccess to remove tags and stuff from data

--- dataprocess/parser.py
+++ dataprocess/parser.py
@@ -1,6 +1,28 @@
 import xml.etree.cElementTree as etree
 
 from dataprocess.models import Post
+from io import StringIO
+from html.parser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+    @staticmethod
+    def strip_tags(html):
+        s = MLStripper()
+        s.feed(html)
+        return s.get_data()
 
 
 class XmlParser(object):
@@ -8,13 +30,16 @@ class XmlParser(object):
         self.CommentsFilePath = CommentsFilePath
         self.postsFilePath = postsFilePath
 
+    def preproccessAttributes(self, post: Post):
+        post.body = MLStripper.strip_tags(post.body)
+        return post
 
     def __iter__(self):
         for event, element in etree.iterparse(self.postsFilePath):
             if element.tag == "row":
-                yield Post(element.attrib)
+                yield self.preproccessAttributes(Post(element.attrib))
 
 
-xmlParser = XmlParser("data\Comments.xml")
+xmlParser = XmlParser("data\Posts.xml")
 for post in xmlParser:
     print(post)
