d9a897fa9abcc82fe5c9f1ce2dd158e1e0f78342
ori_yt <ori_yt@cs.huji.ac.il>
2020-07-17 14:51:21 +0300

    BasicFCN.py - reg + init. hparams.py - max to keep  + reduce main.py - some small modifications. train_and_test.py - named cache + smaller shuffle buffer + smaller lambda-reg

--- hparams.py
+++ hparams.py
@@ -5,6 +5,6 @@ class HParams:
     filePath = path.join("data", "partial", "Posts.xml")
     BATCH_SIZE = 4
     OUTPUT_DIM = 10
-
+    CKPT_MAX_TO_KEEP = 2
 
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from train_and_test import encode, encode_batch
 import numpy as np
 import os
 
+
 def train_partial(*args, **kwargs):
     import train_and_test
     train_and_test.train(*args, **kwargs, dataset_type="partial")
@@ -18,17 +19,22 @@ def train_example(*args, **kwargs):
 def saveIndex():
     xmlParser = XmlParser(HParams.filePath)
     indexPath = os.path.join(os.path.dirname(HParams.filePath), "index")
+    import shutil
+    shutil.rmtree(indexPath, ignore_errors=True)
     hashIndex = Index(indexPath, disk_chunk_size=10)
     for post in xmlParser:
         wordsArr = post.toWordsArray()
-        # assert wordsArr[0] != wordsArr[1]
+        assert wordsArr[0] != wordsArr[1]
         encodedVecs = encode_batch(wordsArr)
-        # assert np.all(encodedVecs[0] != encodedVecs[1])
+        # temp = encodedVecs[0] != encodedVecs[1]
+        # assert np.any(temp)
         postSimHash = np.average(encodedVecs, axis=0)
         hashIndex.insert(post.id, val=postSimHash)
     hashIndex.sort()
 
+
 if __name__ == '__main__':
     # train_example(epochs=1000, restore_last=False, progress_per_step=100)
-    #train_partial(epochs=1000, restore_last=True, progress_per_step=100)
+    # train_partial(epochs=40, epochs_offset=0,
+    #               restore_last=False, progress_per_step=100)
     saveIndex()
