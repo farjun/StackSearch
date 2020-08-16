20c023d79a36d814109d30b4fa587ebf6ca2b821
omer <omerbh1@gmail.com>
2020-08-16 23:03:08 +0300

    added discriminator + GAN train

--- hparams.py
+++ hparams.py
@@ -1,15 +1,11 @@
 from os import path
 
-
 class HParams:
     filePath = path.join("data", "partial", "Posts.xml")
     BATCH_SIZE = 4
     OUTPUT_DIM = 64
     CKPT_MAX_TO_KEEP = 2
     MAX_SENTENCE_DIM = 32
-
-
-
 
     def getCeckpointPath(self):
         return str(self.OUTPUT_DIM)
--- tests/test_search.py
+++ tests/test_search.py
@@ -25,7 +25,7 @@ class MyTestCase(unittest.TestCase):
         self.index.insert(4, self.encoder.encode_batch("maple story good game"))
 
         self.index.index()
-        self.assertEqual(self.index.search(self.encoder.encode_batch("apple pen"), top_k=1), [1])
+        self.assertEqual(self.index.search(self.encoder.encode_batch("apple pen"), top_k=2), [1])
         self.assertEqual(self.index.search(self.encoder.encode_batch("numpy python"), top_k=1), [2])
         self.assertEqual(self.index.search(self.encoder.encode_batch("maple story"), top_k=1), [4])
 
