72f814ed3b2f9223ac686b2de798fda6ae5c1214
omerb <omerbh1@gmail.com>
2020-09-20 13:56:54 +0300

    added test ds option + fixes 2

--- hparams.py
+++ hparams.py
@@ -27,7 +27,7 @@ class HParams:
     def getFeatureExtractor(**kwargs):
         from features.FeatureExtractors import WordEmbeddingToMatrixFeatureExtractor, D2VFeatureExtractor, \
             W2VFeatureExtractor, FeatureExtractor_Temp
-        return FeatureExtractor_Temp(**kwargs)
+        return WordEmbeddingToMatrixFeatureExtractor(**kwargs)
 
     @staticmethod
     def getFeatureExtractorDim():
